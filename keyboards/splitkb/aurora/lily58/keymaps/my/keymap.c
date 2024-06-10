a8279cfb38f9bf50f40f7bffc943a51b9e1bee6f
Alexander Leinich <alexanderleinich1@gmail.com>
2024-06-10 11:10:23 +0200

    Update keymap.c layout for Lily58 keyboard

--- keyboards/splitkb/aurora/lily58/keymaps/my/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/my/keymap.c
@@ -18,10 +18,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[1] = LAYOUT(
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                               KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-        US_EURO, US_DEG, US_ADIA, US_ODIA, US_UDIA, US_SS,                      US_ARNG, US_AE, US_OSTR, KC_LPRN, KC_RPRN, US_NDDR,
+        US_EURO, US_DEG, US_ADIA, US_ODIA, US_UDIA, US_SS,                      US_ARNG, US_AE, US_OSTR, KC_LPRN, KC_RPRN, KC_PIPE,
         KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_TILD,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN,  KC_RPRN, KC_EQL, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, MO(3), KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN,  KC_RPRN, KC_EQL, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, MO(3), KC_DEL, KC_TRNS
         ),
 
 	[2] = LAYOUT(
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_PDOT, KC_P7, KC_P8, KC_P9, KC_PCMM, KC_TRNS,
         KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,                   KC_PEQL, KC_P4, KC_P5, KC_P6, KC_P0, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1, KC_P2, KC_P3, KC_BSLS, KC_TRNS,
-        KC_TRNS, KC_TRNS, MO(3), KC_TRNS,                                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, MO(3), KC_TRNS,                                           KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS
         ),
 
 	[3] = LAYOUT(
--- keyboards/splitkb/aurora/lily58/keymaps/my/readme.md
+++ keyboards/splitkb/aurora/lily58/keymaps/my/readme.md
@@ -1,5 +1,5 @@
 ## generate keymap:
 
 ```bash
-qmk compile -e CONVERT_TO=promicro_rp2040 -kb splitkb/aurora/lily58/rev1 -km my
+    qmk compile -e CONVERT_TO=promicro_rp2040 -kb splitkb/aurora/lily58/rev1 -km my
 ```
