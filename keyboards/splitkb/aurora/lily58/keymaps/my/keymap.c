93b7e46f1d93d4f22753a3e7defaedaaeb3609e7
Alexander Leinich <alexanderleinich1@gmail.com>
2024-06-18 10:58:23 +0200

    Update keymap.c layout for Lily58 keyboard

--- keyboards/splitkb/aurora/lily58/keymaps/my/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/my/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                           KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                           KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
         KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,                          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LGUI,     US_DIAE, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LGUI,     KC_RGUI, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
         KC_LALT, KC_LCTL, MO(1), KC_SPC,                                        KC_ENT, MO(2), KC_BSPC, KC_RGUI
         ),
 
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[2] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_NUM, KC_PPLS, KC_PMNS, KC_PAST, KC_PSLS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_PDOT, KC_P7, KC_P8, KC_P9, KC_PCMM, KC_TRNS,
-        KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,                   KC_PEQL, KC_P4, KC_P5, KC_P6, KC_P0, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1, KC_P2, KC_P3, KC_BSLS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS,                     KC_PDOT, KC_P7, KC_P8, KC_P9, KC_PCMM, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,                   KC_PEQL, KC_P4, KC_P5, KC_P6, KC_P0, KC_TRNS,
+        KC_TRNS, KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1, KC_P2, KC_P3, KC_BSLS, KC_TRNS,
         KC_TRNS, KC_TRNS, MO(3), KC_TRNS,                                           KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS
         ),
 
